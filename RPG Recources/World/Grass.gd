038ac8468eca32ba775863935f84acd372c39739
WorkingCode23 <nickdemidenko32@gmail.com>
2023-08-22 18:31:03 +0200

    P11

--- RPG Recources/World/Grass.gd
+++ RPG Recources/World/Grass.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
-func _process(delta):
-	if Input.is_action_just_pressed("ui_select"):
-		var GrassEffect = load("res://RPG Recources/Effects/GrassEffect.tscn")
-		var grassEffect = GrassEffect.instance() #instance() --> Node erstellen
-		var world = get_tree().current_scene #get_tree() --> root	#current_scene --> der erste Node im root
-		world.add_child(grassEffect)
-		grassEffect.global_position = global_position
-		queue_free()
+func create_grassEffect():
+	var GrassEffect = load("res://RPG Recources/Effects/GrassEffect.tscn")
+	var grassEffect = GrassEffect.instance() #instance() --> Node erstellen
+	var world = get_tree().current_scene #get_tree() --> root	#current_scene --> der erste Node im root
+	world.add_child(grassEffect)
+	grassEffect.global_position = global_position
+
+
+func _on_Hurtbox_area_entered(area):
+	create_grassEffect()
+	queue_free()
